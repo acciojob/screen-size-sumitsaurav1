5f24aed9a2a274edd4dc12392fd73bdd3407a0ad
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-10 12:32:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let height = document.getElementById("width");
+let width = document.getElementById("height")
+
+
+window.addEventListener('resize',(e)=>{
+    width.innerHTML=window.innerWidth;
+    height.innerText=window.innerWidth;
+})
